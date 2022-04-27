434066a691e947a6c5ea240e0c0c266da2ab6171
Daniel Lee <sdlee095@gmail.com>
2022-04-27 10:04:15 -0400

    initial commit

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ console.log(todoItems)
 
 // Use the 'todoItems' variable to create the appropriate elements and append them onto
 // the page.
+
+// incomplete items
+const incompleteList = document.querySelector('#incomplete-items')
+const completeList = document.querySelector('#complete-items')
+
+for (let i=0;i < todoItems.length; i++) {
+    const items = document.createElement('li')
+    items.classList.add('list-group-item')
+    const checkbox = document.createElement('input')
+    checkbox.classList.add('form-check-input')
+    checkbox.type = 'checkbox'
+    if (todoItems[i]["completed"] == false) {
+        items.innerText = todoItems[i]["title"]
+        incompleteList.appendChild(items)
+        checkbox.id = 'flexCheckDefault'
+        items.appendChild(checkbox)
+    } else {
+        items.innerText = todoItems[i]["title"]
+        completeList.appendChild(items)
+        checkbox.id = 'flexCheckChecked'
+        checkbox.setAttribute('checked','')
+        items.appendChild(checkbox)
+    }
+}
